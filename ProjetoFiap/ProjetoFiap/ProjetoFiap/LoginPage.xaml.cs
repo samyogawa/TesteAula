48283c549e425cb61a9583379d6cf641e3905974
Logon Aluno <logonrmlocal@fiap.com.br>
2019-03-19 10:34:07 -0300

    fdsfsdfsd

---
+++ ProjetoFiap/ProjetoFiap/ProjetoFiap/LoginPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProjetoFiap
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+            string[] valores = {
+                "Perfil Arrojado",
+                "Perfil Clássico",
+                "Perfil Arriscado" };
+            ListViewPerfis.ItemsSource = valores;
+
+            BotaoGravar.Clicked += BotaoClicked;
+        }
+
+
+        public async void BotaoClicked(object o, EventArgs e)
+        {
+
+            var mensagemPerfil = "Nenhum perfil selecionado";
+            if (ListViewPerfis.SelectedItem != null)
+                mensagemPerfil = ListViewPerfis.SelectedItem.ToString();
+
+            var mensagem = "Nome: " + EntryNome.Text + "\n" +
+                "Descrição: " + EditorDescricao.Text + "\n" +
+                "Idade: " + SliderIdade.Value.ToString() + "\n " +
+                "Patrimônio: " + StepPatrimonio.Value.ToString() + "\n" +
+                "Perfil: " + mensagemPerfil + " \n ";
+
+            await DisplayAlert("Descrição",mensagem,"Ok");
+
+            
+        }
+    }
+}
